135de07a1b7a4e4bcb3d19d9dcb01d3864ef3d34
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-21 17:47:05 -0500

    Add to BlockChain

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,9 @@
-blockchain = []
+blockchain = [[1]]
 
 def add_value():
-  blockchain.append(5.3)
+  blockchain.append([blockchain[-1], 5.3])
   print(blockchain)
 
+add_value()
+add_value()
 add_value()
