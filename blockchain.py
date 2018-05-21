1885b85b49e81b187485a96b3f1736ea94b0d3b4
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-21 18:06:51 -0500

    function instead of manual constant

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,17 @@
 blockchain = [[1]]
 
 def add_value(transaction_amount):
-  blockchain.append([blockchain[-1], transaction_amount])
+  blockchain.append([get_last_blockchain_value(), transaction_amount])
   print(blockchain)
+
+
+def get_last_blockchain_value():
+  blockchain[-1]
+
 
 add_value(2)
 add_value(0.9)
 add_value(10.89)
+
+
+
