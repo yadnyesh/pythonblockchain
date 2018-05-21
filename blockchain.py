2b97dda7a380d59c4202154f3eae1432416220ef
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-21 18:09:23 -0500

    Another commit

--- blockchain.py
+++ blockchain.py
@@ -1,12 +1,11 @@
 blockchain = [[1]]
 
+def get_last_blockchain_value():
+  return blockchain[-1]
+
 def add_value(transaction_amount):
   blockchain.append([get_last_blockchain_value(), transaction_amount])
   print(blockchain)
-
-
-def get_last_blockchain_value():
-  blockchain[-1]
 
 
 add_value(2)
