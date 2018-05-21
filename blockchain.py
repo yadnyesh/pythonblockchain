32d13375587ed0d5be0638549df7d76c340ba993
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-21 18:16:16 -0500

    #28 - Default Arguments

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,16 @@
-blockchain = [[1]]
+blockchain = [10]
 
 def get_last_blockchain_value():
   return blockchain[-1]
 
-def add_value(transaction_amount):
-  blockchain.append([get_last_blockchain_value(), transaction_amount])
+def add_value(transaction_amount, last_transaction=get_last_blockchain_value()):
+  blockchain.append([last_transaction, transaction_amount])
   print(blockchain)
 
 
-add_value(2)
-add_value(0.9)
-add_value(10.89)
+add_value(2, [1])
+add_value(0.9, get_last_blockchain_value())
+add_value(10.89, get_last_blockchain_value())
 
 
 
