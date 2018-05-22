629c751f86bf376c8007c575f56425a75961f703
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-21 19:18:44 -0500

    input using function

--- blockchain.py
+++ blockchain.py
@@ -7,8 +7,10 @@ def add_value(transaction_amount, last_transaction=get_last_blockchain_value()):
   blockchain.append([last_transaction, transaction_amount])
   print(blockchain)
 
+def get_user_input():
+  return float(input('Please enter the transaction amount: '))
 
-tx_amount = input('Please enter the transaction amount: ')
+tx_amount = get_user_input()
 add_value(float(tx_amount), [1])
 add_value(0.9, get_last_blockchain_value())
 add_value(10.89, get_last_blockchain_value())
