2d03bf231ee5ff8c9b4f22d3bf83d720f8e3138a
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-06-22 21:55:41 +0530

    Formatted Code

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,22 @@
 blockchain = [10]
 
+
 def get_last_blockchain_value():
-  return blockchain[-1]
+    return blockchain[-1]
+
 
 def add_value(transaction_amount, last_transaction=get_last_blockchain_value()):
-  blockchain.append([last_transaction, transaction_amount])
-  print(blockchain)
+    blockchain.append([last_transaction, transaction_amount])
+    print(blockchain)
+
 
 def get_user_input():
-  return float(input('Please enter the transaction amount: '))
+    return float(input('Please enter the transaction amount: '))
+
 
 tx_amount = get_user_input()
 add_value(float(tx_amount), [1])
 add_value(0.9, get_last_blockchain_value())
+
+
 add_value(10.89, get_last_blockchain_value())
